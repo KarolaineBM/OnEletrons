29e85ad5d00c95eb96f4ccfceb2fbe5258096d2e
KarolaineBM <karolainebarrosm@gmail.com>
2024-04-17 09:44:55 -0300

    Imagens e modificações iniciais

---
+++ src/pages/LanddingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page>
+    <div class="row">
+      <img class="full-width" src="~/assets/img1.png" />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LanddingPage",
+});
+</script>
