676f2f38622d29422759ca547be5d32c7c2971c9
KarolaineBM <karolainebarrosm@gmail.com>
2024-05-02 08:22:11 -0300

    COmentários + estrutura row e col

--- src/pages/LanddingPage.vue
+++ src/pages/LanddingPage.vue
@@ -1,7 +1,68 @@
 <template>
-  <q-page>
+  <q-page class="q-pa-md">
+    <!-- IMAGEM + NOME DA EMPRESA -->
     <div class="row">
       <img class="full-width" src="~/assets/img1.png" />
+    </div>
+
+    <!-- CARDS COM INFORMAÇÕES -->
+    <div class="row">
+      <div class="col-md-6">
+        <div class="row"></div>
+        <div class="row"></div>
+      </div>
+
+      <div class="col-md-6">
+        <div class="row"></div>
+        <div class="row"></div>
+      </div>
+    </div>
+
+    <!-- INFORMAÇÕES SOBRE ENTREGAS -->
+    <div class="row">
+      <div class="col-md-12">
+        <div class="row"></div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="row"></div>
+            <div class="row"></div>
+            <div class="row"></div>
+            <div class="row"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- TROCA DE SUCATA POR DESCONTO -->
+    <div class="row">
+      <div class="col-md-12">
+        <div class="row"></div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="row"></div>
+            <div class="row"></div>
+            <div class="row"></div>
+            <div class="row"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- ACESSE NOSSO SITE -->
+    <div class="row">
+      <div class="col-md-6"></div>
+      <div class="col-md-6">
+        <div class="row"></div>
+        <div class="row"></div>
+      </div>
+    </div>
+
+    <!-- FALE CONOSCO -->
+    <div class="row">
+      <div class="col-md-12">
+        <div class="row"></div>
+        <div class="row"></div>
+      </div>
     </div>
   </q-page>
 </template>
